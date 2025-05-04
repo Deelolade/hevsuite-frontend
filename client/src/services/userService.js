23794c19b83a61d118b0316a6eb129e700024395
bikkibro <bibektimilsina7857@gmail.com>
2025-05-04 20:45:57 +0545

    isauthenticated login for protected route

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -5,12 +5,9 @@ import axios from 'axios';
 const baseUrl = process.env.REACT_APP_API_BASE_URL; 
 const API_URL = `${baseUrl}/api/user`;
 
-const getUserProfile = async (token) => {
+const getUserProfile = async () => {
   try {
     const response = await axios.get(`${API_URL}/profile`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
       withCredentials: true,
     });
 
