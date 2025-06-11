9c6de232962bcfbc045c85721a5fb640a37f400b
Isaac Katongole <katongolelsaac78@gmail.com>
2025-06-11 18:56:48 +0300

    fix: Purge cached redux store on app init

--- client/src/index.js
+++ client/src/index.js
@@ -7,20 +7,25 @@ import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-      <Toaster
-        toastOptions={{
-          position: 'top-right',
-          style: {
-            background: 'white',
-            color: 'black',
-          },
-        }}
-      />
-    </PersistGate>
-  </Provider>
-);
+persistor.purge().then(()=> {
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster
+          toastOptions={{
+            position: 'top-right',
+            style: {
+              background: 'white',
+              color: 'black',
+            },
+          }}
+        />
+      </PersistGate>
+    </Provider>
+  );
+
+  }
+)
