11b354eb242c6dd08ee70cc870473e3d42a3630d
omotayobolu <omotayodaniel10@gmail.com>
2025-06-15 22:38:21 +0100

    fix: topics and faq data integrated with API

--- client/src/services/topicsService.js
+++ client/src/services/topicsService.js
@@ -1,13 +1,12 @@
+// services/userService.js
+import axios from "axios";
 
-  // services/userService.js
-import axios from 'axios';
-
-const baseUrl = process.env.REACT_APP_API_BASE_URL; 
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
 const API_URL = `${baseUrl}/api/`;
 
 const fetchVisibleTopics = async () => {
   try {
-    const response = await axios.get(`${API_URL}topics/visible`);
+    const response = await axios.get(`${API_URL}topic/visible`);
     console.log(response); // Log the response data for debugging
     return {
       success: true,
@@ -34,27 +33,26 @@ const fetchVisibleTopicById = async (id) => {
     };
   }
 };
-const fetchFAQs= async () => {
-    try {
-      const response = await axios.get(`${API_URL}faqs/visible`);
-      console.log(response); // Log the response data for debugging
-      return {
-        success: true,
-        data: response.data,
-      };
-    } catch (error) {
-      return {
-        success: false,
-        error: error.response?.data?.message || error.message,
-      };
-    }
-  };
-  
+const fetchFAQs = async () => {
+  try {
+    const response = await axios.get(`${API_URL}faq/visible`);
+    console.log(response); // Log the response data for debugging
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.response?.data?.message || error.message,
+    };
+  }
+};
 
-const topicsService= {
-    fetchVisibleTopics,
-    fetchVisibleTopicById,
-    fetchFAQs
+const topicsService = {
+  fetchVisibleTopics,
+  fetchVisibleTopicById,
+  fetchFAQs,
 };
 
 export default topicsService;
